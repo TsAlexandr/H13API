5add7e6cac92d52f1d29de16dfe15738f8f91482
devliss <liseyna1@gmail.com>
2022-11-03 09:37:33 +0300

    added vercel.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,29 +7,28 @@ import { BlogsModule } from './blogs/blogs.module';
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { UsersModule } from './users/users.module';
-import * as mongoose from "mongoose";
-import { EmailModule } from "./emailManager/emailModule";
-
-const mongoURI = "mongodb+srv://devliss:devlissadmin@incubator.pz501up.mongodb.net/ht_03?retryWrites=true&w=majority"//process.env.mongoURI || 'mongodb://localhost:27017';
+import * as mongoose from 'mongoose';
+import { EmailModule } from './emailManager/emailModule';
 
 mongoose.set('toJSON', {
   virtuals: true,
   transform: (doc, converted) => {
     delete converted._id;
-  }
+  },
 });
+
 @Module({
   imports: [
-
-    MongooseModule.forRoot(mongoURI),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRoot(process.env.mongoURI),
     BlogsModule,
     PostsModule,
     CommentsModule,
     UsersModule,
-    EmailModule],
+    EmailModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { BadRequestException, ValidationPipe } from "@nestjs/common";
-import { HttpExceptionFilter } from "./exception.filter";
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './exception.filter';
 async function bootstrap() {
-
-  console.log(process.env.PORT)
+  console.log(process.env.PORT);
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-  exceptionFactory:(errors)=>{
-
-      const errorsForResp = []
-    errors.forEach((err)=>{
-      const keys = Object.keys(err.constraints)
-      keys.forEach(k=>{
-        errorsForResp.push({message:err.constraints[k], field:err.property})
-      })
-    })
-      //throw new BadRequestException(errors.map(e=>{return {field:e.property}}))
-  }
-  }));
-  app.enableCors()
-  app.useGlobalFilters(new HttpExceptionFilter())
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const errorsForResp = [];
+        errors.forEach((err) => {
+          const keys = Object.keys(err.constraints);
+          keys.forEach((k) => {
+            errorsForResp.push({
+              message: err.constraints[k],
+              field: err.property,
+            });
+          });
+        });
+        //throw new BadRequestException(errors.map(e=>{return {field:e.property}}))
+      },
+    }),
+  );
+  app.enableCors();
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT || 7000);
 }
 bootstrap();

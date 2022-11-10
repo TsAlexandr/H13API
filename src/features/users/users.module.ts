ba747123cbec2b91b63a462163758c0c6beb58e4
devliss <liseyna1@gmail.com>
2022-11-10 19:27:50 +0300

    v14 added middleware for registration

--- src/features/users/users.module.ts
+++ src/features/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { UsersService } from './application/users.service';
 import { UsersController } from './api/users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -6,6 +6,7 @@ import { User, UserSchema } from './entities/user.schema';
 import { UserQueryRepository } from './infrastructure/user-query.repository';
 import { UserRepository } from './infrastructure/user.repository';
 import { EmailModule } from '../../emailManager/emailModule';
+import { CheckExistingUserMiddleware } from '../../common/middlewares/existUser.middleware';
 
 @Module({
   controllers: [UsersController],
@@ -14,6 +15,12 @@ import { EmailModule } from '../../emailManager/emailModule';
     EmailModule,
   ],
   providers: [UsersService, UserQueryRepository, UserRepository],
-  exports: [UsersService,UserQueryRepository,UserRepository],
+  exports: [UsersService, UserQueryRepository, UserRepository],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(CheckExistingUserMiddleware)
+      .forRoutes({ path: 'posts/registration', method: RequestMethod.POST });
+  }
+}

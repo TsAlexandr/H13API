30c687ac2a0f4e216e9996d5b628ad952e6fc646
devliss <liseyna1@gmail.com>
2022-11-11 21:32:16 +0300

    ht_14 fixed name validation in createBlogSto

--- src/features/blogs/dto/createBlog.dto.ts
+++ src/features/blogs/dto/createBlog.dto.ts
@@ -1,6 +1,9 @@
-import { IsUrl, Length } from 'class-validator';
+import { IsNotEmpty, IsUrl, Length } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class CreateBlogDto {
+  @IsNotEmpty()
+  @Transform(({ value }) => value?.trim())
   @Length(1, 15)
   public name: string;
 

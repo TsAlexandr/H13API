4db82168f684fdc5080a3d372f5329aa184616d6
devliss <liseyna1@gmail.com>
2022-11-12 10:01:22 +0300

    ht_14 fixed trim post dto

--- src/features/posts/dto/createPost.dto.ts
+++ src/features/posts/dto/createPost.dto.ts
@@ -1,15 +1,24 @@
-import { Length } from 'class-validator';
+import { IsNotEmpty, Length } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class CreatePostDto {
+  @IsNotEmpty()
+  @Transform(({ value }) => value?.trim())
   @Length(1, 30)
   title: string;
 
+  @IsNotEmpty()
+  @Transform(({ value }) => value?.trim())
   @Length(1, 1000)
   content: string;
 
+  @IsNotEmpty()
+  @Transform(({ value }) => value?.trim())
   @Length(1, 100)
   shortDescription: string;
 
+  @IsNotEmpty()
+  @Transform(({ value }) => value?.trim())
   @Length(1)
   blogId: string;
 }
